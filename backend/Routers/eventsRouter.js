ce961ff83d244f72d0bcab87582db7f7bc3ecc29
Usman Naeem <usman_chaudhary1@yahoo.com>
2024-09-28 18:05:05 +0500

    removed comments

--- backend/Routers/eventsRouter.js
+++ backend/Routers/eventsRouter.js
@@ -14,9 +14,5 @@ router.use(authController.restrictTo("events", "admin"));
 router.patch("/updateEvent/:id", eventsController.updateEvent);
 router.post("/addEvent", eventsController.addEvent);
 router.delete("/dropEvent/:id", eventsController.dropEvent);
- /* whats up. its me hasso */ 
- // Hello I am watching valo stream
- // kaam krle, events page is done.
- // Need to improve
- // yes, waiting for gpt to recharge
+
 module.exports = router;
